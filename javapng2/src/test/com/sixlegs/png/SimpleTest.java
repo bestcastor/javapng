8f85e38307ab97a8b3322ddce91313b5841e90f5
herbyderby <herbyderby@8812c3b3-2733-0410-91f1-5953a5dbc411>
2005-03-10 02:57:57 +0000

    add error tests

--- javapng2/src/test/com/sixlegs/png/SimpleTest.java
+++ javapng2/src/test/com/sixlegs/png/SimpleTest.java
@@ -9,11 +9,37 @@ extends TestCase
     public void testRead()
     throws Exception
     {
-        PngImage png = new PngImage();
-        InputStream in = getClass().getResourceAsStream("/images/misc/cc1.png");
-        png.read(in, true);
+        PngImage png = readResource("/images/misc/cc1.png");
         assertEquals(138, png.getWidth());
         assertEquals(180, png.getHeight());
+    }
+
+    public void testErrors()
+    throws Exception
+    {
+        errorHelper("/images/suite/x00n0g01.png");
+        errorHelper("/images/suite/xcrn0g04.png");
+        errorHelper("/images/suite/xlfn0g04.png");
+    }
+
+    public void errorHelper(String path)
+    throws Exception
+    {
+        try {
+            readResource(path);
+            fail("Expected exception");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private PngImage readResource(String path)
+    throws IOException
+    {
+        PngImage png = new PngImage();
+        InputStream in = getClass().getResourceAsStream(path);
+        png.read(in, true);
+        return png;
     }
 
     public SimpleTest(String name)
