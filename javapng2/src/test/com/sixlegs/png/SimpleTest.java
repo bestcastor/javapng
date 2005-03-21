cf3f7ba07c73338809e55f4bcfb93e83352bba4e
herbyderby <herbyderby@8812c3b3-2733-0410-91f1-5953a5dbc411>
2005-03-21 01:06:41 +0000

    msOG, sPLT examples

--- javapng2/src/test/com/sixlegs/png/SimpleTest.java
+++ javapng2/src/test/com/sixlegs/png/SimpleTest.java
@@ -1,11 +1,51 @@
 package com.sixlegs.png;
 
+import java.awt.image.BufferedImage;
 import java.io.*;
 import junit.framework.*;
 
 public class SimpleTest
 extends TestCase
 {
+    public void testPrivateChunk()
+    throws Exception
+    {
+        PngImage png = new PngImage(new MyPngConfig());
+        png.read(getClass().getResourceAsStream("/images/misc/anigif.png"), true);
+        byte[] bytes = (byte[])png.getProperty(MyPngConfig.ORIGINAL_GIF);
+
+        assertEquals("MSOFFICE9.0", new String(bytes, 0, 11, "US-ASCII"));
+        
+        File file = File.createTempFile("msog", ".gif");
+        OutputStream out = new FileOutputStream(file);
+        out.write(bytes, 11, bytes.length - 11);
+        out.close();
+
+        BufferedImage image = javax.imageio.ImageIO.read(file);
+        assertEquals(32, image.getWidth());
+        assertEquals(32, image.getHeight());        
+    }
+
+    private static class MyPngConfig
+    extends BasicPngConfig
+    {
+        private static final String ORIGINAL_GIF = "original_gif";
+        private static final int msOG = PngChunk.stringToType("msOG");
+
+        private static final PngChunk CHUNK = new PngChunk(msOG){
+            public void read(PngInputStream in, PngImage png) throws IOException {
+                byte[] bytes = new byte[in.getRemaining()];
+                in.readFully(bytes);
+                png.getProperties().put(ORIGINAL_GIF, bytes);
+            }
+        };
+        
+        public PngChunk getChunk(int type)
+        {
+            return (type == msOG) ? CHUNK : super.getChunk(type);
+        }
+    }
+
     public void testRead()
     throws Exception
     {
