05f79df7e02c9995320a959170d0245616146968
herbyderby <herbyderby@8812c3b3-2733-0410-91f1-5953a5dbc411>
2005-05-15 09:41:26 +0000

    update benchmark to load pngs using toolkit and imageio also

--- javapng2/src/test/com/sixlegs/png/Benchmark.java
+++ javapng2/src/test/com/sixlegs/png/Benchmark.java
@@ -21,27 +21,76 @@ Boston, MA  02111-1307, USA.
 package com.sixlegs.png;
 
 import com.sixlegs.png.ext.*;
+import java.awt.*;
 import java.io.*;
+import java.util.*;
+import java.util.List;
+import javax.imageio.ImageIO;
 
 public class Benchmark
 {
+    abstract static class PngReader
+    {
+        abstract public void read(File file) throws IOException;
+    }
+
+    private static PngReader SIXLEGS = new PngReader(){
+        private PngConfig config = new BasicPngConfig();
+        public void read(File file) throws IOException {
+            new PngImage(config).read(file);
+        }
+    };
+
+    private static PngReader IMAGEIO = new PngReader(){
+        public void read(File file) throws IOException {
+            ImageIO.read(file);
+        }
+    };
+
+    private static PngReader TOOLKIT = new PngReader(){
+        private Toolkit toolkit = Toolkit.getDefaultToolkit();
+        private MediaTracker tracker = new MediaTracker(new Component(){});
+        public void read(File file) throws IOException {
+            try {
+                tracker.addImage(toolkit.createImage(file.toURL()), 0);
+                tracker.waitForAll();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    };
+    
     public static void main(String[] args)
     throws Exception
     {
         int loop = (args.length > 0) ? Integer.parseInt(args[0]) : 1;
-        ExtendedPngConfig config = new ExtendedPngConfig();
-        config.setWarningsFatal(true);
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        int count = 0;
-        long t = System.currentTimeMillis();
+        List list = new ArrayList();
         String line;
-        while ((line = r.readLine()) != null) {
+        while ((line = r.readLine()) != null)
+            list.add(new File(line));
+        File[] files = (File[])list.toArray(new File[list.size()]);
+        benchmark(files, loop, SIXLEGS, "Sixlegs");
+        benchmark(files, loop, IMAGEIO, "ImageIO");
+        benchmark(files, loop, TOOLKIT, "Toolkit");
+    }
+
+    private static void benchmark(File[] files, int loop, PngReader reader, String desc)
+    throws IOException
+    {
+        File cur = null;
+        try {
+            long t = System.currentTimeMillis();
             for (int i = 0; i < loop; i++) {
-                count++;
-                new PngImage(config).read(new File(line));
+                for (int j = 0; j < files.length; j++) {
+                    reader.read(cur = files[j]);
+                }
             }
+            t = System.currentTimeMillis() - t;
+            System.err.println(desc + ": read " + (files.length * loop) + " images in " + t + " ms");
+        } catch (IOException e) {
+            System.err.println("Error reading " + cur);
+            throw e;
         }
-        t = System.currentTimeMillis() - t;
-        System.err.println("Read " + count + " images in " + t + " ms");
     }
 }
