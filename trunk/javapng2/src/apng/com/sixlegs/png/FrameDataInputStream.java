97b731639dc92de03648db20ce1bc108e77d629f
herbyderby <herbyderby@8812c3b3-2733-0410-91f1-5953a5dbc411>
2007-03-07 17:26:30 +0000

    finish apng impl

--- trunk/javapng2/src/apng/com/sixlegs/png/FrameDataInputStream.java
+++ trunk/javapng2/src/apng/com/sixlegs/png/FrameDataInputStream.java
@@ -37,19 +37,100 @@ exception statement from your version.
 package com.sixlegs.png;
 
 import java.io.*;
-import java.util.List;
+import java.util.*;
 
 class FrameDataInputStream
 extends InputStream
 {
+    private final RandomAccessFile file;
+    private final Iterator it;
+    private InputStream in;
+
     public FrameDataInputStream(File file, List frameData)
+    throws IOException
     {
-        // TODO
+        this.file = new RandomAccessFile(file, "r");
+        this.it = frameData.iterator();
+        advance();
+    }
+
+    public void close()
+    throws IOException
+    {
+        if (in != null) {
+            in.close();
+            in = null;
+            while (it.hasNext())
+                it.next();
+        }
+        file.close();
+    }
+
+    private void advance()
+    throws IOException
+    {
+        if (in != null)
+            in.close();
+        in = null;
+        if (it.hasNext()) {
+            // TODO: enable streaming
+            FrameData data = (FrameData)it.next();
+            file.seek(data.getOffset());
+            byte[] bytes = new byte[data.getLength()];
+            file.readFully(bytes);
+            in = new ByteArrayInputStream(bytes);
+        }
+    }
+        
+    public int available()
+    throws IOException
+    {
+        if (in == null)
+            return 0;
+        return in.available();
+    }
+
+    public boolean markSupported()
+    {
+        return false;
     }
 
     public int read()
     throws IOException
     {
-        return -1;
+        if (in == null)
+            return -1;
+        int result = in.read();
+        if (result == -1) {
+            advance();
+            return read();
+        }
+        return result;
+    }
+    
+    public int read(byte[] b, int off, int len)
+    throws IOException
+    {
+        if (in == null)
+            return -1;
+        int result = in.read(b, off, len);
+        if (result == -1) {
+            advance();
+            return read(b, off, len);
+        }
+        return result;
+    }
+
+    public long skip(long n)
+    throws IOException
+    {
+        if (in == null)
+            return 0;
+        long result = in.skip(n);
+        if (result != 0)
+            return result;
+        if (read() == -1)
+            return 0;
+        return 1;
     }
 }
